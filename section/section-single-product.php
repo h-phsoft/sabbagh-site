a84d430c12f00199046e44f6e16fbb0150ca3375
nscc- <nscc-@192.168.1.187>
2026-06-29 16:23:05 +0300

    By Haytham

--- section/section-single-product.php
+++ section/section-single-product.php
@@ -24,9 +24,12 @@ $oCat = cEcomCat::getInstance($oProduct->CatId);
       <div class="col-md-6">
         <h1 class="mb-3"><a href="products/<?= $oCat->Id ?>#cat-<?= $oCat->Id ?>"><?= $vLang == 'en' ? $oCat->Name1 : $oCat->Name2 ?></a></h1>
         <h2 class="mb-3"> <?= $vLang == 'en' ? $oProduct->Name1 : $oProduct->Name2 ?></h2>
+        <h5 class="mb-3">INGREDIENTS</h5>
         <p class="text-muted mb-4"><?= $oProduct->Desc1 ?></p>
         <p class="text-muted mb-4"><?= $oProduct->Desc2 ?></p>
+        <h5 class="mb-3">Caution</h5>
         <p class="text-muted mb-4"><?= $oProduct->Desc3 ?></p>
+        <h5 class="mb-3">Storage Conditions</h5>
         <p class="text-muted mb-4"><?= $oProduct->Desc4 ?></p>
         <p class="text-muted mb-4"><?= $oProduct->Desc5 ?></p>
       </div>
